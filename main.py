26578a6ed3eda047e7244fd9c6a82a6e9835b1ce
Miloš Zeljko <miloszeljko00@gmail.com>
2022-10-19 23:28:10 +0200

    v1.0.0

---
+++ main.py
@@ -0,0 +1,97 @@
+from models import *
+from jinja2 import Template
+import os
+
+data = {
+    "class": {
+        "name": "User",
+        "properties": [
+            {
+                "type": "long",
+                "name": "Id"
+            },
+            {
+                "type": "string",
+                "name": "Username"
+            },
+            {
+                "type": "string",
+                "name": "Password"
+            },
+            {
+                "type": "IEnumerable<Room>",
+                "name": "Rooms"
+            }
+        ]
+    }
+}
+
+
+def generate_model():
+    exists = os.path.exists(current_directory + "\\model\\")
+    if not exists:
+        os.makedirs("model")
+
+    model_file = open(current_directory + "\\model\\" + data["class"]["name"] + ".cs", "w")
+
+    j2_template = Template(model_template)
+    model_file.write(j2_template.render(data))
+
+    model_file.close()
+
+
+def generate_repository():
+    exists = os.path.exists(current_directory + "\\repository\\")
+    if not exists:
+        os.makedirs("repository")
+
+    repository_interface_file = open(current_directory + "\\repository\\I" + data["class"]["name"]+"Repository.cs", "w")
+
+    j2_template = Template(repository_interface_template)
+    repository_interface_file.write(j2_template.render(data))
+
+    repository_interface_file.close()
+
+    repository_file = open(current_directory + "\\repository\\" + data["class"]["name"] + "Repository.cs", "w")
+
+    j2_template = Template(repository_template)
+    repository_file.write(j2_template.render(data))
+
+    repository_file.close()
+
+
+def generate_service():
+    exists = os.path.exists(current_directory + "\\service\\")
+    if not exists:
+        os.makedirs("service")
+
+    repository_interface_file = open(current_directory + "\\service\\I" + data["class"]["name"]+"Service.cs", "w")
+
+    j2_template = Template(service_interface_template)
+    repository_interface_file.write(j2_template.render(data))
+
+    repository_interface_file.close()
+
+    repository_file = open(current_directory + "\\service\\" + data["class"]["name"] + "Service.cs", "w")
+
+    j2_template = Template(service_template)
+    repository_file.write(j2_template.render(data))
+
+    repository_file.close()
+
+
+if __name__ == '__main__':
+    current_directory = os.getcwd()
+
+    try:
+        print("Generating model...")
+        generate_model()
+        print("Generating repository...")
+        generate_repository()
+        print("Generating service...")
+        generate_service()
+        raise Exception("greskadsdadsa")
+    except Exception as e:
+        print("Something else went wrong :(")
+        print("\n\nERORR:\n")
+        print(e)
---
+++ models.py
@@ -0,0 +1,150 @@
+model_template = """
+using System;
+
+namespace HospitalLibrary.Core.Model
+{
+    public class {{ class.name }}
+    {
+    
+        {% for property in class.properties -%}
+        public {{ property.type }} {{ property.name }} { get; set; }
+        
+        {% endfor %}
+    }
+}
+"""
+
+repository_interface_template ="""
+using HospitalLibrary.Core.Model;
+using System.Collections.Generic;
+
+namespace HospitalLibrary.Core.Repository
+{
+    public interface I{{class.name}}Repository
+    {
+        IEnumerable<{{class.name}}> GetAll();
+        {{class.name}} GetById({{class.properties[0].type}} {{class.properties[0].name.lower()}});
+        void Create({{class.name}} {{class.name.lower()}});
+        void Update({{class.name}} {{class.name.lower()}});
+        void Delete({{class.name}} {{class.name.lower()}});
+    }
+}
+"""
+
+repository_template ="""
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Settings;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HospitalLibrary.Core.Repository
+{
+    public class {{class.name}}Repository : I{{class.name}}Repository
+    {
+        private readonly HospitalDbContext _context;
+
+        public {{class.name}}Repository(HospitalDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<{{class.name}}> GetAll()
+        {
+            return _context.{{class.name}}s.ToList();
+        }
+
+        public {{class.name}} GetById({{class.properties[0].type}} {{class.properties[0].name.lower()}})
+        {
+            return _context.{{class.name}}s.Find({{class.properties[0].name.lower()}});
+        }
+
+        public void Create({{class.name}} {{class.name.lower()}})
+        {
+            _context.{{class.name}}s.Add({{class.name.lower()}});
+            _context.SaveChanges();
+        }
+
+        public void Update({{class.name}} {{class.name.lower()}})
+        {
+            _context.Entry({{class.name.lower()}}).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                throw;
+            }
+        }
+
+        public void Delete({{class.name}} {{class.name.lower()}})
+        {
+            _context.{{class.name}}s.Remove({{class.name.lower()}});
+            _context.SaveChanges();
+        }
+    }
+}
+"""
+
+service_interface_template="""
+using HospitalLibrary.Core.Model;
+using System.Collections.Generic;
+
+namespace HospitalLibrary.Core.Service
+{
+    public interface I{{class.name}}Service
+    {
+        IEnumerable<{{class.name}}> GetAll();
+        {{class.name}} GetById({{class.properties[0].type}} {{class.properties[0].name.lower()}});
+        void Create({{class.name}} {{class.name.lower()}});
+        void Update({{class.name}} {{class.name.lower()}});
+        void Delete({{class.name}} {{class.name.lower()}});
+    }
+}
+"""
+
+service_template="""
+using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Repository;
+using System.Collections.Generic;
+
+namespace HospitalLibrary.Core.Service
+{
+    public class {{class.name}}Service : I{{class.name}}Service
+    {
+        private readonly I{{class.name}}Repository _{{class.name.lower()}}Repository;
+
+        public {{class.name}}Service(I{{class.name}}Repository {{class.name.lower()}}Repository)
+        {
+            _{{class.name.lower()}}Repository = {{class.name.lower()}}Repository;
+        }
+
+        public IEnumerable<{{class.name}}> GetAll()
+        {
+            return _{{class.name.lower()}}Repository.GetAll();
+        }
+
+        public {{class.name}} GetById({{class.properties[0].type}} {{class.properties[0].name.lower()}})
+        {
+            return _{{class.name.lower()}}Repository.GetById({{class.properties[0].name.lower()}});
+        }
+
+        public void Create({{class.name}} {{class.name.lower()}})
+        {
+            _{{class.name.lower()}}Repository.Create({{class.name.lower()}});
+        }
+
+        public void Update({{class.name}} {{class.name.lower()}})
+        {
+            _{{class.name.lower()}}Repository.Update({{class.name.lower()}});
+        }
+
+        public void Delete({{class.name}} {{class.name.lower()}})
+        {
+            _{{class.name.lower()}}Repository.Delete({{class.name.lower()}});
+        }
+    }
+}
+"""
